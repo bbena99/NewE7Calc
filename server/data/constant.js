fac48f21e6d61b1648ff294d716020f8b1101f04
Brock Bena <bbena99@gmail.com>
2024-08-05 18:38:43 -0500

    Added all Fire Warriors

--- server/data/constant.js
+++ server/data/constant.js
@@ -19,6 +19,215 @@ const charList = [
       'Dual Attack Chance' : 3
     }
   },{
+    'name' : 'Benimaru',
+    'nameNoSpace' : 'Benimaru',
+    'element' : 0,
+    'class' : 0,
+    'sign' : 8,
+    'engraveStat':7,
+    'engrave':[0,5.6,8.4,11.2,14,16.8],
+    'base_stats':{
+      'Attack'  : 1177,
+      'Defense' : 553,
+      'Health'  : 5542,
+      'Speed'   : 119,
+      'Critical Hit Chance' : 27,
+      'Critical Hit Damage' : 150,
+      'Effectiveness' : 0,
+      'Effect Resistance' : 0,
+      'Dual Attack Chance' : 3
+    }
+  },{
+    'name' : 'Cermia',
+    'nameNoSpace' : 'Cermia',
+    'element' : 0,
+    'class' : 0,
+    'sign' : 4,
+    'engraveStat':3,
+    'engrave':[0,6,9,12,15,18],
+    'base_stats':{
+      'Attack'  : 1359,
+      'Defense' : 585,
+      'Health'  : 5542,
+      'Speed'   : 105,
+      'Critical Hit Chance' : 23,
+      'Critical Hit Damage' : 150,
+      'Effectiveness' : 0,
+      'Effect Resistance' : 0,
+      'Dual Attack Chance' : 3
+    }
+  },{
+    'name' : 'Edward Elric',
+    'nameNoSpace' : 'Edward-Elric',
+    'element' : 0,
+    'class' : 0,
+    'sign' : 0,
+    'engraveStat':7,
+    'engrave':[0,5.6,8.4,11.2,14,16.8],
+    'base_stats':{
+      'Attack'  : 984,
+      'Defense' : 637,
+      'Health'  : 6266,
+      'Speed'   : 117,
+      'Critical Hit Chance' : 15,
+      'Critical Hit Damage' : 150,
+      'Effectiveness' : 0,
+      'Effect Resistance' : 0,
+      'Dual Attack Chance' : 3
+    }
+  },{
+    'name' : 'Holiday Yufine',
+    'nameNoSpace' : 'Holiday-Yufine',
+    'element' : 0,
+    'class' : 0,
+    'sign' : 6,
+    'engraveStat':3,
+    'engrave':[0,6,9,12,15,18],
+    'base_stats':{
+      'Attack'  : 1119,
+      'Defense' : 627,
+      'Health'  : 6266,
+      'Speed'   : 109,
+      'Critical Hit Chance' : 15,
+      'Critical Hit Damage' : 150,
+      'Effectiveness' : 0,
+      'Effect Resistance' : 0,
+      'Dual Attack Chance' : 3
+    }
+  },{
+    'name' : 'Hwayoung',
+    'nameNoSpace' : 'Hwayoung',
+    'element' : 0,
+    'class' : 0,
+    'sign' : 6,
+    'engraveStat':3,
+    'engrave':[0,6,9,12,15,18],
+    'base_stats':{
+      'Attack'  : 1119,
+      'Defense' : 627,
+      'Health'  : 6266,
+      'Speed'   : 109,
+      'Critical Hit Chance' : 15,
+      'Critical Hit Damage' : 150,
+      'Effectiveness' : 0,
+      'Effect Resistance' : 0,
+      'Dual Attack Chance' : 3
+    }
+  },{
+    'name' : 'Jack-O\'',
+    'nameNoSpace' : 'Jack-O',
+    'element' : 0,
+    'class' : 0,
+    'sign' : 1,
+    'engraveStat':3,
+    'engrave':[0,6,9,12,15,18],
+    'base_stats':{
+      'Attack'  : 1228,
+      'Defense' : 553,
+      'Health'  : 5784,
+      'Speed'   : 109,
+      'Critical Hit Chance' : 23,
+      'Critical Hit Damage' : 165,
+      'Effectiveness' : 0,
+      'Effect Resistance' : 0,
+      'Dual Attack Chance' : 3
+    }
+  },{
+    'name' : 'Kane',
+    'nameNoSpace' : 'Kane',
+    'element' : 0,
+    'class' : 0,
+    'sign' : 4,
+    'engraveStat':3,
+    'engrave':[0,6,9,12,15,18],
+    'base_stats':{
+      'Attack'  : 1359,
+      'Defense' : 585,
+      'Health'  : 5542,
+      'Speed'   : 105,
+      'Critical Hit Chance' : 23,
+      'Critical Hit Damage' : 150,
+      'Effectiveness' : 0,
+      'Effect Resistance' : 0,
+      'Dual Attack Chance' : 3
+    }
+  },{
+    'name' : 'Ken',
+    'nameNoSpace' : 'Ken',
+    'element' : 0,
+    'class' : 0,
+    'sign' : 9,
+    'engraveStat':4,
+    'engrave':[0,6,9,12,15,18],
+    'base_stats':{
+      'Attack'  : 966,
+      'Defense' : 657,
+      'Health'  : 7323,
+      'Speed'   : 102,
+      'Critical Hit Chance' : 15,
+      'Critical Hit Damage' : 150,
+      'Effectiveness' : 0,
+      'Effect Resistance' : 0,
+      'Dual Attack Chance' : 3
+    }
+  },{
+    'name' : 'Ravi',
+    'nameNoSpace' : 'Ravi',
+    'element' : 0,
+    'class' : 0,
+    'sign' : 9,
+    'engraveStat':5,
+    'engrave':[0,6,9,12,15,18],
+    'base_stats':{
+      'Attack'  : 966,
+      'Defense' : 657,
+      'Health'  : 7323,
+      'Speed'   : 102,
+      'Critical Hit Chance' : 15,
+      'Critical Hit Damage' : 150,
+      'Effectiveness' : 0,
+      'Effect Resistance' : 0,
+      'Dual Attack Chance' : 3
+    }
+  },{
+    'name' : 'Shalltear',
+    'nameNoSpace' : 'Shalltear',
+    'element' : 0,
+    'class' : 0,
+    'sign' : 8,
+    'engraveStat':7,
+    'engrave':[0,5.6,8.4,11.2,14,16.8],
+    'base_stats':{
+      'Attack'  : 1177,
+      'Defense' : 553,
+      'Health'  : 5542,
+      'Speed'   : 119,
+      'Critical Hit Chance' : 27,
+      'Critical Hit Damage' : 150,
+      'Effectiveness' : 0,
+      'Effect Resistance' : 0,
+      'Dual Attack Chance' : 3
+    }
+  },{
+    'name' : 'Sol',
+    'nameNoSpace' : 'Sol',
+    'element' : 0,
+    'class' : 0,
+    'sign' : 8,
+    'engraveStat': 5,
+    'engrave':[0,6,9,12,15,18],
+    'base_stats':{
+      'Attack'  : 1177,
+      'Defense' : 553,
+      'Health'  : 5542,
+      'Speed'   : 119,
+      'Critical Hit Chance' : 27,
+      'Critical Hit Damage' : 150,
+      'Effectiveness' : 0,
+      'Effect Resistance' : 0,
+      'Dual Attack Chance' : 3
+    }
+  },{
     'name' : 'Abyssal Yufine',
     'nameNoSpace' : 'Abyssal-Yufine',
     'element' : 4,
